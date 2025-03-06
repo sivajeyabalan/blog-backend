74108690a3064873205905d3b48d4c3007f81fa8
SIVAJB <121388913+sivajeyabalan@users.noreply.github.com>
2025-03-06 19:30:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors"); // Import cors
 require("dotenv").config(); // Import dotenv
 app.use(express.json()); // Use express.json
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "*" }));
 app.get("/favicon.ico", (req, res) => res.status(204).end());
 
 app.use("/uploads", express.static("uploads"));
