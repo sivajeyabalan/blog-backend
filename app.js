f694618cbaab7150089f67b8f6d9f5d565438075
SIVAJB <121388913+sivajeyabalan@users.noreply.github.com>
2025-03-07 11:22:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const cors = require("cors"); // Import cors
 require("dotenv").config(); // Import dotenv
 app.use(express.json()); // Use express.json
 
-app.use(cors({
-  origin: 'https://blogapi-sadl.onrender.com', // Must be a specific origin, not '*'
-  credentials: true
-}));
+app.use(cors());
 
 app.get("/favicon.ico", (req, res) => res.status(204).end());
 
